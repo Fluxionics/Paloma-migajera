f2da0e234207b2ba7142eb53a5ab405bc5d2052d
Guillermo <memitoyt1356@gmail.com>
2026-02-14 08:00:02 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const gameConfig = {
-    nombre: "PALOMA MIGAJERA",
-    version: "v1.0.2",
-    estado: "Alpha", // O "Beta", "Demo", etc.
-    desarrollador: "Guillermo R. - Francisco D."
-    
-};
